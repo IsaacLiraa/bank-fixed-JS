c60b2ec5541fdba8ca60630d7b4c27095d42566f
IsaacLiraa <instancememo@gmail.com>
2025-01-31 10:40:46 -0400

    Adding Radius

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type='button' className={`font-[poppins] font-medium text-[18px] text-black outline-none py-4 px-6 bg-blue-gradient ${styles}`}>
+    <button type='button' className={`rounded-xl font-[poppins] font-medium text-[18px] text-black outline-none py-4 px-6 bg-blue-gradient ${styles}`}>
       Get Started
     </button>
   )
