cb138330ba03b5c938429b21ab00d68c3ae6c234
IsaacLiraa <instancememo@gmail.com>
2025-01-31 12:28:42 -0400

    Added CTA Section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import styles from "../style"
+import Button from "./Button"
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section id="CTA" className={`w-full ${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex flex-col flex-1">
+        <h2 className={styles.heading2}>Let’s try our service now!
+
+        </h2>
+        <p className={`max-w-[470px] mt-5 ${styles.paragraph}`}>
+          Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+        </p>
+      </div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
   )
 }
 
