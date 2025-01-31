4923a1bf03596e8db571320f21ef8bbf0beb9a5b
IsaacLiraa <instancememo@gmail.com>
2025-01-31 12:01:25 -0400

    Added Client Section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section id="clients" className={`my-4 ${styles.flexCenter}`}>
+      <div className="flex flex-wrap flex-row flex-1 justify-between items-center w-full">
+        {clients.map((client) => (
+          <div key={client.id} className={`sm:min-w-[192px] min-w-[120px]`}>
+            <img className="sm:w-[192px] w-[100px] object-contain" src={client.logo} alt="client" />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
